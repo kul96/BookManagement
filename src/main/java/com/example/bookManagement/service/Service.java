fc4e6979b89748e0fa046fdccd6f8db154de597e
kul96 <kuldeepsuthar96@gmail.com>
2025-08-03 13:01:30 +0530

    updated logger with lombok

--- src/main/java/com/example/bookManagement/service/Service.java
+++ src/main/java/com/example/bookManagement/service/Service.java
@@ -7,8 +7,7 @@ import com.example.bookManagement.exception.DuplicateDataFoundException;
 import com.example.bookManagement.exception.TitleFoundNullException;
 import com.example.bookManagement.repository.Repo;
 import com.example.bookManagement.util.Mapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -18,8 +17,10 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 @org.springframework.stereotype.Service
+@Slf4j
 public class Service {
-    private static final Logger logger = LoggerFactory.getLogger(Service.class);
+//    private static final Logger log = LoggerFactory.getLogger(Service.class);
+// no need if @Slf4j is used . Auto generated same code at compile time
 
     private Repo repository;
     private Mapper mapper;
@@ -32,7 +33,7 @@ public class Service {
     }
 
     public BookDTO addBook(BookDTO bookDTO) {
-        logger.info("Adding book with name: {} ", bookDTO.getTitle());
+        log.info("Adding book with name: {} ", bookDTO.getTitle());
         if (bookDTO.getTitle() == null || bookDTO.getTitle()
                                                  .isBlank()) {
             throw new TitleFoundNullException("Value of Title should not be null/empty");
@@ -45,12 +46,12 @@ public class Service {
             throw new DuplicateDataFoundException("Tile of Book '" + bookDTO.getTitle() + "' is Duplicate.");
         }
 //        repository.addBook(book); // pass Book
-        logger.info("Book added with id : {}", save.getId());
+        log.info("Book added with id : {}", save.getId());
         return mapper.mapBookToBookDTO(save);
     }
 
     public BookDTO getBook(String title) {
-        logger.info("Fetching book with name : {}", title);
+        log.info("Fetching book with name : {}", title);
         if (title == null || title.isBlank()) {
             throw new TitleFoundNullException("Title should not be empty");
         }
@@ -62,19 +63,19 @@ public class Service {
     }
 
     public int deleteBook(String title) {
-        logger.info("Deleting book with name : {}", title);
+        log.info("Deleting book with name : {}", title);
         if (title == null || title.isBlank()) {
             throw new TitleFoundNullException("Title should not be empty");
         }
             var row = repository.deleteByTitle(title);
             if(row == 0)
                 throw new BookNotFoundException("Book with title '" + title + "' not found.");
-            logger.info("Book deleted with name : {}", title);
+            log.info("Book deleted with name : {}", title);
             return row;
     }
 
     public int updateBook(BookDTO bookDTO) {
-        logger.info("Updating book with name : {}", bookDTO.getTitle());
+        log.info("Updating book with name : {}", bookDTO.getTitle());
         if (bookDTO.getTitle() == null || bookDTO.getTitle()
                                                  .isBlank()) {
             throw new TitleFoundNullException("Value of Title should not be null/empty");
@@ -88,7 +89,7 @@ public class Service {
             if (rows == 0) { // todo dedicated exception
                 throw new RuntimeException("Enter valid Data/ Data not match");
             }
-            logger.info("Book updated with name : {}", bookDTO.getTitle());
+            log.info("Book updated with name : {}", bookDTO.getTitle());
             return rows;
         } catch (DataIntegrityViolationException e) {
             throw new DuplicateDataFoundException("Value of Title '" + bookDTO.getTitle() + "' is duplicate.");
@@ -96,7 +97,7 @@ public class Service {
     }
 
     public List<BookDTO> getAllBooks() {
-        logger.info("Fetching all book.");
+        log.info("Fetching all book.");
         List<Book> books = repository.findAll();
         if (books.isEmpty()) throw new BookNotFoundException("No Book found");
 //        List<BookDTO> bookDTOS = books.stream()
@@ -106,14 +107,14 @@ public class Service {
     }
 
     public Page<BookDTO> pageGetAllBooks(Pageable pageable) {
-        logger.info("Fetching all book with page");
+        log.info("Fetching all book with page");
         Page<Book> bookPage = repository.findAll(pageable);
         if (bookPage.isEmpty()) throw new BookNotFoundException("No Book found");
         return bookPage.map(mapper::mapBookToBookDTO);
     }
 
     public List<BookDTO> getBookByAuthor(String author) {
-        logger.info("Fetching book with author : {}", author);
+        log.info("Fetching book with author : {}", author);
         if (author == null || author.isBlank()) {
             throw new TitleFoundNullException("Author should not be empty");
         }
@@ -127,7 +128,7 @@ public class Service {
     }
 
     public List<BookDTO> getByCriteria(String title, String author, String price) {
-        logger.info("Fetching book with criteria  name :{} , author:{}, price:{}", title, author, price);
+        log.info("Fetching book with criteria  name :{} , author:{}, price:{}", title, author, price);
         Integer parsedPrice = null;
         try {
             if (price != null && !price.isBlank()) {
